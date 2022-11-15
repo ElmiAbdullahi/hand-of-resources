da3c86bde00004afaa551999979050330df93330
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-15 10:58:08 -0800

    started table setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS subaru;
+
+CREATE TABLE subaru (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    model VARCHAR NOT NULL,
+    type VARCHAR NOT NULL,
+    year INT NOT NULL
+
+);
+
+INSERT INTO subaru(
+    model,
+    type,
+    year
+)
+
+
