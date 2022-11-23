b61feaf01a213f86fc016b52f8c20d84a61ec677
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-22 18:04:32 -0800

    GET /subaru route built

---
+++ __tests__/subaru.test.js
@@ -0,0 +1,51 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('subaru routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET /subaru should return list of subarus', async () => {
+    const resp = await request(app).get('/subaru');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "model": "BRZ",
+          "type": "coupe",
+          "year": 2017,
+        },
+        Object {
+          "id": "2",
+          "model": "Outback",
+          "type": "wagon",
+          "year": 2021,
+        },
+        Object {
+          "id": "3",
+          "model": "WRX STI",
+          "type": "sedan",
+          "year": 2015,
+        },
+        Object {
+          "id": "4",
+          "model": "Crosstrek XV",
+          "type": "sedan",
+          "year": 2020,
+        },
+        Object {
+          "id": "5",
+          "model": "Forester",
+          "type": "SUV",
+          "year": 2019,
+        },
+      ]
+    `);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -16,4 +16,23 @@ INSERT INTO subaru(
     year
 )
 
+VALUES 
+('BRZ', 'coupe', 2017),
+('Outback', 'wagon', 2021),
+('WRX STI', 'sedan', 2015),
+('Crosstrek XV', 'sedan', 2020),
+('Forester', 'SUV', 2019);
+
+-- Table #2
+-- DROP TABLE IF EXISTS toyota;
+
+-- CREATE TABLE toyota (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     model VARCHAR NOT NULL,
+--     type VARCHAR NOT NULL,
+--     year INT NOT NULL
+
+-- );
+
+-- INSERT INFO
 
